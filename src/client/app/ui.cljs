d914c606db1ecc8e23fef5fef11a5fdc1bbc73a8
Tony Kay <twkay@thenavisway.com>
2016-06-22 10:57:43 -0700

    UI with initial state

--- src/client/app/ui.cljs
+++ src/client/app/ui.cljs
@@ -4,10 +4,48 @@
             yahoo.intl-messageformat-with-locales
             [untangled.client.core :as uc]))
 
-(defui ^:once Root
+(defui ^:once Item
+  static uc/InitialAppState
+  (initial-state [clz {:keys [label]}] {:label label})
+  static om/IQuery
+  (query [this] [:label])
+  static om/Ident
+  (ident [this {:keys [label]}] [:items/by-label label])
   Object
   (render [this]
-    (dom/div nil
-      (dom/h4 nil "Header")
-      (dom/p nil "TODO"))))
+    (let [{:keys [label]} (om/props this)]
+      (dom/li nil label))))
 
+(def ui-item (om/factory Item {:keyfn :label}))
+
+(defui ^:once MyList
+  static uc/InitialAppState
+  (initial-state [clz params] {:title "Initial List"
+                               :items [(uc/initial-state Item {:label "A"})
+                                       (uc/initial-state Item {:label "C"})
+                                       (uc/initial-state Item {:label "B"})]})
+  static om/IQuery
+  (query [this] [:title {:items (om/get-query Item)}])
+  static om/Ident
+  (ident [this {:keys [title]}] [:lists/by-title title])
+  Object
+  (render [this]
+    (let [{:keys [title items]} (om/props this)]
+      (dom/div nil
+        (dom/h4 nil title)
+        (dom/ul nil
+          (map ui-item items))))))
+
+(def ui-list (om/factory MyList))
+
+(defui ^:once Root
+  static uc/InitialAppState
+  (initial-state [clz params] {:list (uc/initial-state MyList {})})
+  static om/IQuery
+  (query [this] [:ui/react-key {:list (om/get-query MyList)}])
+  Object
+  (render [this]
+    (let [{:keys [ui/react-key list]} (om/props this)]
+      (dom/div #js {:key react-key}
+        (dom/h4 nil "Header")
+        (ui-list list)))))
