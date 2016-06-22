6c694461f3320d8bbea0923e7265a130230354b5
Tony Kay <twkay@thenavisway.com>
2016-06-22 10:14:14 -0700

    slight bit more cleanup

--- dev/client/cljs/user.cljs
+++ dev/client/cljs/user.cljs
@@ -1,12 +1,9 @@
 (ns cljs.user
   (:require
     [app.core :refer [app]]
-    [untangled.client.core :as core]
     [cljs.pprint :refer [pprint]]
     [devtools.core :as devtools]
-    [untangled.client.logging :as log]
-    [untangled.client.impl.util :refer [log-app-state]]
-    [app.ui :as ui]))
+    [untangled.client.impl.util :as util]))
 
 (enable-console-print!)
 
@@ -14,5 +11,6 @@
 (devtools/enable-feature! :sanity-hints)
 (devtools/install!)
 
-; Mount the app and remember it.
-(reset! app (core/mount @app ui/Root "app"))
+(def log-app-state (partial util/log-app-state app))
+
+
--- src/client/app/core.cljs
+++ src/client/app/core.cljs
@@ -1,7 +1,9 @@
 (ns app.core
   (:require
     app.mutations
-    [untangled.client.core :as uc]))
+    [untangled.client.core :as uc]
+    [app.ui :as ui]))
 
 (defonce app (atom (uc/new-untangled-client)))
+(reset! app (uc/mount @app ui/Root "app"))
 
